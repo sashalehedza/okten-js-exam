205fa795d32cc94b6c36a951ce149a65e3b8e56f
sashalehedza <sashalehedza@gmail.com>
2024-06-02 09:25:16 +0300

    fix

--- post-details.js
+++ post-details.js
@@ -8,37 +8,48 @@
 // блоки з короткою іфною про post - в ряд по 5 .
 // post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
 // Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки (дати фон. марджини і тд)
+const wrapperDiv = document.createElement('div')
+wrapperDiv.classList.add('wrapper')
+document.body.appendChild(wrapperDiv)
+
+const containerDiv = document.createElement('div')
+containerDiv.classList.add('container')
+wrapperDiv.append(containerDiv)
 
 const postId = new URLSearchParams(window.location.search).get('id')
 fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
   .then((response) => response.json())
   .then((post) => {
-    const block = document.createElement('div')
-    block.classList.add('post')
+    const postDiv = document.createElement('div')
+    postDiv.classList.add('post')
     const id = document.createElement('p')
     id.textContent = post.id
-    const title = document.createElement('p')
+    const title = document.createElement('h3')
     title.textContent = post.title
     const body = document.createElement('p')
     body.textContent = post.body
-    block.append(id, title, body)
-    document.body.appendChild(block)
+    postDiv.append(id, title, body)
+    containerDiv.append(postDiv)
   })
   .catch((error) => console.log(error))
 
 fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
   .then((response) => response.json())
   .then((comments) => {
+    const commentsDiv = document.createElement('div')
+    commentsDiv.classList.add('comments')
+    containerDiv.append(commentsDiv)
     comments.forEach((comment) => {
-      const commentBlock = document.createElement('div')
+      const commentDiv = document.createElement('div')
+      commentDiv.classList.add('comment')
       const name = document.createElement('p')
       name.textContent = comment.name
       const email = document.createElement('p')
       email.textContent = comment.email
       const body = document.createElement('p')
       body.textContent = comment.body
-      commentBlock.append(name, email, body)
-      document.body.appendChild(commentBlock)
+      commentDiv.append(name, email, body)
+      commentsDiv.append(commentDiv)
     })
   })
   .catch((error) => console.log(error))
